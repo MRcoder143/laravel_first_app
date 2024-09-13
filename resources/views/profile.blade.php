4b3f66f2d6494a22b64c3a5c65ee50feb7a36dee
moeezmubashar2 <moeezmubashar2@gmail.com>
2024-09-13 20:18:11 +0500

    upload tamplate to laravel

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.masterLayout')
+@section('content')
+    <h1>this is profile page</h1>
+@endsection
+
+@section('title')
+    Profile
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,81 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::get('/profile', function () {
+    return view('profile');
 });
+
+
+
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+// Route::view('/post', 'post');
+
+//Parameter value 
+// Route::get('/post/{id?}', function ($id = null) {
+//     if($id){
+// return "<h1>Post id is .'$id'.</h1>";
+//}else
+//{
+//return "no value found";
+//}
+// });
+
+//Multiple Parameter value 
+// Route::get('/post/{id?/comment/{cmttd?}}', function ($id = null, $cmttd = null) {
+//      if($id){
+// return "<h1>Post id is .'$id'.</h1>";
+//}else
+//{
+//return "no value found";
+//}    
+// });
+
+// routes constraints
+// Route::get('/post/{id?}', function ($id = null) {
+//      if($id){
+// return "<h1>Post id is $id</h1>";
+// }else
+// {
+// return "no value found";
+// }    
+// })->whereNumber('id');
+
+
+// multiple constraints
+// Route::get('/post/{id?}/comment/{cmtid?}', function ($id = null, $cmtid = null) {
+//      if($id){
+// return "<h1>Post id is $id and comment id is $cmtid</h1>";
+// }else
+// {
+// return "no value found";
+// }    
+// })->whereNumber('id')->whereAlpha('cmtid');
+
+
+
+// named routes
+
+// Route::get('/post-page/{id?}', function ($id = null) {
+//      if($id){
+// return "<h1>Post id is $id</h1>";
+// }else
+// {
+// return "no value found";
+// }    
+// })->whereNumber('id')->name('post');
+
+
+
+
+
+
